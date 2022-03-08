cb680fbe584b3193ccba73ce7e0cf151f47c32f1
Dong-min Kwak <kdm0324@naver.com>
2022-03-08 14:13:41 +0900

    Add Delete

--- src/components/Chirstweet.js
+++ src/components/Chirstweet.js
@@ -1,10 +1,21 @@
+import { dbService } from "fbase";
+
 const Christweet = ({ christweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+      const ok = window.confirm("삭제하시겠습니까?");
+      console.log(ok);
+      if(ok) {
+          console.log(christweetObj.id);
+          const data = await dbService.doc(`christweets/${christweetObj.id}`).delete();
+          console.log(data);
+      }
+  };
   return (
     <div>
       <h4>{christweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Christweet</button>
+          <button onClick={onDeleteClick}>Delete Christweet</button>
           <button>Edit Chirstweet</button>
         </>
       )}
